880994a9cb2effde6c593b94ed8711c0b701c74d
vallit0 <evalleusac@gmail.com>
2025-06-05 00:16:37 -0600

    add: tabla de simbolos para analisis lexico

--- Clase1/compiler/main.go
+++ Clase1/compiler/main.go
@@ -1,82 +1,67 @@
 package main
 
 import (
-	// Ajusta si tu módulo se llama diferente
 	"fmt"
+	"os"
 
 	compiler "compiler/parser"
+
+	//"main/cst"
+	"compiler/errors"
+	// "main/repl"
 
 	"github.com/antlr4-go/antlr/v4"
 )
 
-/*
-Go no provee una estructura directa para crear clases
-asi que cuando lo manejamos de manera interna,
-solamente podemos utilizar unas funciones y hacerles un estilo
-de attachment por medio de las funciones func (v)
-*/
-type EvalVisitor struct {
-	*compiler.BaseV4LangGrammarVisitor
-}
-
-func (v *EvalVisitor) Visit(tree antlr.ParseTree) interface{} {
-	return tree.Accept(v)
-}
-
-func (v *EvalVisitor) VisitProg(ctx *compiler.ProgContext) interface{} {
-	return v.Visit(ctx.Expr())
-}
-
-func (v *EvalVisitor) VisitAddSub(ctx *compiler.AddSubContext) interface{} {
-	left := v.Visit(ctx.Expr(0)).(int)
-	right := v.Visit(ctx.Expr(1)).(int)
-
-	switch ctx.GetOp().GetTokenType() {
-	case compiler.V4LangLexerPLUS:
-		return left + right
-	case compiler.V4LangLexerMINUS:
-		return left - right
-	}
-	return 0
-}
-
-func (v *EvalVisitor) VisitMulDiv(ctx *compiler.MulDivContext) interface{} {
-	left := v.Visit(ctx.Expr(0)).(int)
-	right := v.Visit(ctx.Expr(1)).(int)
-
-	switch ctx.GetOp().GetTokenType() {
-	case compiler.V4LangLexerMUL:
-		return left * right
-	case compiler.V4LangLexerDIV:
-		return left / right
-	}
-	return 0
-}
-
-func (v *EvalVisitor) VisitInt(ctx *compiler.IntContext) interface{} {
-	var value int
-	fmt.Sscanf(ctx.GetText(), "%d", &value)
-	return value
-}
-
-func (v *EvalVisitor) VisitParens(ctx *compiler.ParensContext) interface{} {
-	return v.Visit(ctx.Expr())
-}
-
 func main() {
-	input := antlr.NewInputStream("3 + 4 * (2 - 1)")
+	// Leer código fuente desde archivo o consola
+	fmt.Println("Escribe el codigo de Vlang: (Ctrl+D) para terminar")
+	inputCode, err := readStdin()
+	if err != nil {
+		fmt.Println("Error leyendo entrada:", err)
+		return
+	}
 
-	lexer := compiler.NewV4LangLexer(input)
-	tokens := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	//startTime := time.Now()
 
-	parser := compiler.NewV4LangGrammar(tokens)
+	// Canal para generar CST en paralelo (opcional)
+	/*
+		resultChannel := make(chan string)
+		go func() {
+			resultChannel <- cst.CstReport(inputCode)
+		}()
+	*/
+
+	// 1. Análisis Léxico
+	// Para verificar errores
+	lexicalErrorListener := errors.NewLexicalErrorListener()
+	//
+	lexer := compiler.NewV4LangLexer(antlr.NewInputStream(inputCode))
+
+	lexer.RemoveErrorListeners()
+	lexer.AddErrorListener(lexicalErrorListener)
+
+	// 2. Tokens
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	// 3. Parser + errores sintácticos
+	// New<Nombre de mi gramatica>(Stream)
+	parser := compiler.NewV4LangGrammar(stream)
+	parser.BuildParseTrees = true
+
+	syntaxErrorListener := errors.NewSyntaxErrorListener(lexicalErrorListener.ErrorTable)
 	parser.RemoveErrorListeners()
-	parser.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+	parser.SetErrorHandler(errors.NewCustomErrorStrategy())
+	parser.AddErrorListener(syntaxErrorListener)
 
-	tree := parser.Prog()
+	// 4. Árbol sintáctico
+	// En tu gramatica tienes el axioma, o simbolo inicial
+	// Este es el que deberas agregar como parte del parser.
+	//tree := parser.Program()
 
-	visitor := &EvalVisitor{}
-	result := visitor.Visit(tree)
+}
 
-	fmt.Println("Resultado:", result)
+func readStdin() (string, error) {
+	input, err := os.ReadFile("/dev/stdin")
+	return string(input), err
 }
