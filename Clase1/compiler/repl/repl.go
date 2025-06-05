68d3b21bd5ec198a742040ecfd251f35c849d56f
vallit0 <evalleusac@gmail.com>
2025-06-05 00:20:46 -0600

    fix: tabla de simbolos

--- Clase1/compiler/repl/repl.go
+++ Clase1/compiler/repl/repl.go
@@ -4,9 +4,9 @@ type ReplContext struct {
 	// The console is the output of the REPL
 	Console *Console
 	// The scope is the current scope of the REPL
-	ScopeTrace *ScopeTrace
+	//ScopeTrace *ScopeTrace
 	// The call stack is the stack of breakable, continueable and returnable items
-	CallStack *CallStack
+	//CallStack *CallStack
 	// Error table is the table of errors
 	ErrorTable *ErrorTable
 }
